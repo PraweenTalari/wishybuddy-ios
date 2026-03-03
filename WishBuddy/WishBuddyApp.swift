42bfd2793b05121274633298fac77b4a206669d8
praveen <praween.talari@gmail.com>
2026-03-03 11:05:47 +0530

    Initial Commit

---
+++ WishBuddy/WishBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishBuddyApp.swift
+//  WishBuddy
+//
+//  Created by Praveen Talari on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct WishBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
